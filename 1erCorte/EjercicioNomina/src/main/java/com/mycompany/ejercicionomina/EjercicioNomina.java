c5bd0448dce5307054cc64b0e9b169fea1ff2c57
danielgonzalez909887 <199773217+danielgonzalez909887@users.noreply.github.com>
2025-03-11 19:08:18 -0500

    Subo cambios

---
+++ 1erCorte/EjercicioNomina/src/main/java/com/mycompany/ejercicionomina/EjercicioNomina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicionomina;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class EjercicioNomina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
